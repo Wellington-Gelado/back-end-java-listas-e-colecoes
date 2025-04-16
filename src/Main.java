fd91090f68866e621af4de091a07635e3a7d6aab
F9823694 Wellington Santos Ferreira <wellington.ferreira@bb.com.br>
2025-04-16 14:19:32 -0300

    Desafio aula 1 concluído.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
+
+        Pessoa pessoa1 = new Pessoa("Wellington" , 29);
+        Pessoa pessoa2 = new Pessoa("Alana" , 23);
+        Pessoa pessoa3 = new Pessoa("Eliza" , 0);
+
+        listaDePessoas.add(pessoa1);
+        listaDePessoas.add(pessoa2);
+        listaDePessoas.add(pessoa3);
+
+        System.out.println("Tamanho da lista de pessoas = " + listaDePessoas.size());
+
+        System.out.println("A primeira pessoa da lista é : " + listaDePessoas.getFirst());
+
+        System.out.println("Imprimindo lista de objetos...");
+        System.out.println(listaDePessoas);
+
+        System.out.println("Imprimindo objetos da lista...");
+        for (Pessoa pessoa : listaDePessoas){
+            System.out.println(pessoa);
+        }
     }
 }
