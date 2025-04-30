f441f959ec0745488d7c61776e7943a8db4a7e93
F9823694 Wellington Santos Ferreira <wellington.ferreira@bb.com.br>
2025-04-30 17:43:23 -0300

    Desafio 3.4 v2 (Uso de Stream)

--- src/MainProduto.java
+++ src/MainProduto.java
@@ -7,8 +7,8 @@ public class MainProduto {
     public static void main(String[] args) {
         ArrayList<Produto> listaDeProdutos = new ArrayList<>();
 
-        BigDecimal valorTotal = BigDecimal.ZERO;
-        BigDecimal media = BigDecimal.ZERO;
+//        BigDecimal valorTotal = BigDecimal.ZERO;
+//        BigDecimal media = BigDecimal.ZERO;
 
         Produto produto1 = new Produto("Arroz Integral" , BigDecimal.valueOf(20.45), 3);
         Produto produto2 = new Produto("Whey Protein" , BigDecimal.valueOf(50), 8);
@@ -27,15 +27,25 @@ public class MainProduto {
 
         System.out.println("Imprimindo objetos da lista...");
         for (Produto produto : listaDeProdutos){
-            valorTotal = valorTotal.add(produto.getPreco());
+//            valorTotal = valorTotal.add(produto.getPreco());
             System.out.println(produto);
         }
 
         if (listaDeProdutos.isEmpty()){
             System.out.println("Lista Vazia, não existe média.");
         }else{
-            media = valorTotal.divide(new BigDecimal(listaDeProdutos.size()), 2, RoundingMode.HALF_UP);
-            System.out.println("Média da lista = " +media+ "!");
+//            media = valorTotal.divide(new BigDecimal(listaDeProdutos.size()), 2, RoundingMode.HALF_UP);
+//            System.out.println("Média da lista = " +media+ "!");
+
+            BigDecimal valorTotal = listaDeProdutos.stream()
+                    .map(Produto::getPreco)
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+            BigDecimal media = (listaDeProdutos.size() > 0)
+                    ? valorTotal.divide(BigDecimal.valueOf(listaDeProdutos.size()), 2, RoundingMode.HALF_UP)
+                    : BigDecimal.ZERO;
+
+            System.out.println("Média dos preços: " + media);
         }
 
 
