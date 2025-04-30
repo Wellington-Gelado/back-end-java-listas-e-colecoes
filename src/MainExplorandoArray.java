20ec2507952773b2826e87369e4176871e71ece2
F9823694 Wellington Santos Ferreira <wellington.ferreira@bb.com.br>
2025-04-30 13:43:49 -0300

    Desafio 3.1

---
+++ src/MainExplorandoArray.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class MainExplorandoArray {
+
+    public static void main(String[] args) {
+        ArrayList<String> listaNomes= new ArrayList<>();
+
+        String nome1 = new String("Wellington");
+        String nome2 = new String("Alana");
+        String nome3 = new String("Eliza");
+
+        listaNomes.add(nome1);
+        listaNomes.add(nome2);
+        listaNomes.add(nome3);
+
+        for(String nome : listaNomes){
+            System.out.println("Nome: " + nome);
+        }
+    }
+}
