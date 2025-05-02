50d19136072e9c6637fa576b9edc294fc729460e
F9823694 Wellington Santos Ferreira <wellington.ferreira@bb.com.br>
2025-05-02 13:27:36 -0300

    Desafio 3.6

---
+++ src/ContaBancaria.java
@@ -0,0 +1,17 @@
+public class ContaBancaria {
+    private int conta;
+    private double saldo;
+
+    ContaBancaria (int conta, double saldo){
+        this.conta = conta;
+        this.saldo = saldo;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
---
+++ src/MainContaBancaria.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class MainContaBancaria {
+    public static void main(String[] args) {
+        List<ContaBancaria> contasBancarias= new ArrayList<>();
+
+        ContaBancaria conta1 = new ContaBancaria(123, 250.26);
+        ContaBancaria conta2 = new ContaBancaria(456, 550.56);
+        ContaBancaria conta3 = new ContaBancaria(789, 10.26);
+
+        contasBancarias.add(conta1);
+        contasBancarias.add(conta2);
+        contasBancarias.add(conta3);
+
+        System.out.println("Descobrindo conta com maior saldo - TRADICIONAL");
+        ContaBancaria contaMaiorSaldo = contasBancarias.getFirst();
+        for (ContaBancaria conta : contasBancarias){
+            if (conta.getSaldo() > contaMaiorSaldo.getSaldo()){
+                contaMaiorSaldo = conta;
+            }
+        }
+
+        System.out.println("""
+                A conta %d tem o maior saldo = %.2f!!
+                """.formatted(contaMaiorSaldo.getConta(), contaMaiorSaldo.getSaldo()));
+
+        System.out.println("Descobrindo conta com maior saldo - STREAM");
+
+        ContaBancaria contaMaiorSaldoStream = contasBancarias.stream()
+                .max(Comparator.comparingDouble(ContaBancaria::getSaldo))
+                .orElseThrow(() -> new NoSuchElementException("Lista de contas está vazia!"));
+
+        System.out.println("""
+                A conta %d tem o maior saldo = %.2f!!
+                """.formatted(contaMaiorSaldo.getConta(), contaMaiorSaldo.getSaldo()));
+
+
+    }
+}
